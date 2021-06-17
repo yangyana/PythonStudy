7acb3c1edb342c07de48e7e8ad11874583fe9559
yangyana <1642092466@qq.com>
2021-06-17 18:43:42 +0800

    main

--- AAA.py
+++ AAA.py
@@ -1,2 +1,14 @@
 
-    print("222,yyy")
+    file            文本文件类型  后期放配置文件、日志
+    directory       文件夹
+    package         包  图标上有个点
+    python file     用于目前写代码
+    html file       用于放测试报告
+
+
+
+
+#文件夹：        放静态资源 测试报告 测试赛数据 截图 日志
+#包 package     同类性质的代码 放到package里面
+#测试用例代码放test_case，公用代码放public包里
+#package:    有_inti_.py文件  暂时空着
